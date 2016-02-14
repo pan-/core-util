fb79031b309014180f83eb062ad7234594a57e52
Vincent Coubard <vincent.coubard@arm.com>
2016-02-14 20:45:10 +0000

    Add append meta function, which apend two type sequences together.

---
+++ core-util/meta/append.hpp
@@ -0,0 +1,23 @@
+#ifndef MBED_META_APPEND_H_
+#define MBED_META_APPEND_H_
+
+
+namespace mbed { namespace meta {
+
+template<typename L, typename R>
+struct append;
+
+template<
+    template<typename...> class LeftContainer, typename... LeftTs,
+    template<typename...> class RightContainer, typename... RightTs
+>
+struct append<LeftContainer<LeftTs...>, RightContainer<RightTs...>> {
+	using type = LeftContainer<LeftTs..., RightTs...>;
+};
+
+template<typename L, typename R>
+using append_t = typename append<L, R>::type;
+
+}} // namespace mbed::meta
+
+#endif /* MBED_META_APPEND_H_ */
